63ca105a738aab4487a6f600d176246bc5369963
dengrenjie <dengrenjie31@gmail.com>
2018-03-16 23:12:07 +0800

    Lab4: Stack done

---
+++ Lab4/queue.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+bool CreateQueue(Queue *queue, int size) {//Create a new queue
+	if (size <= 0)//Check if the size is valid
+		return false;
+	queue->values = (double *)malloc(sizeof(double) * size);//Alloc space for queue
+	queue->front = 0;
+	queue->rear = -1;
+	queue->counter = 0;
+	queue->maxSize = size;
+	return true;
+}
+
+bool IsEmpty(Queue *queue) {//Check if the queue is empty
+	return queue->counter == 0;
+}
+
+bool IsFull(Queue *queue) {//Check if the queue is full
+	return queue->counter >= queue->maxSize;
+}
+
+bool Enqueue(Queue *queue, double x) {//Add element into the queue
+	if (IsFull(queue))//If the queue is full, stop adding elements into it
+		return false;
+	queue->rear = (queue->rear + 1) % queue->maxSize;//Find the position of queue's tail
+	queue->values[queue->rear] = x;
+	queue->counter++;
+	return true;
+}
+
+bool Dequeue(Queue *queue, double *x) {//Delete the first element in the queue
+	if (IsEmpty(queue))//If the queue is empty, stop deleting elements in it
+		return false;
+	queue->front = (queue->front + 1) % queue->maxSize;//Find the position of queue's front
+	*x = queue->values[queue->front];
+	queue->counter--;
+	return true;
+}
+
+void DisplayQueue(Queue *queue) {//Display the queue
+	printf("front -->%8g\n", queue->values[queue->front]);
+	if (queue->front > queue->rear) {
+		for (int i = queue->front + 1; i < queue->maxSize; i++) {
+			printf("%17g\n", queue->values[i]);
+		}
+		for (int i = 0; i < queue->rear; i++) {
+			printf("%17g\n", queue->values[i]);
+		}
+	}
+	else {
+		for (int i = queue->front + 1; i < queue->rear; i++) {
+			printf("%17g\n", queue->values[i]);
+		}
+	}
+	printf("%17g%17s\n", queue->values[queue->rear], "<-- rear");
+}
+
+void DestroyQueue(Queue *queue) {
+	free(queue->values);
+}
---
+++ Lab4/queue.h
@@ -0,0 +1,42 @@
+typedef struct {
+	double *values;
+	int front;
+	int rear;
+	int counter;
+	int maxSize;
+} Queue;
+
+bool CreateQueue(Queue *queue, int size);
+/*
+- Creates an empty queue whose capacity is size
+*/
+
+bool IsEmpty(Queue *queue);
+/*
+- Return true if the queue is full
+*/
+
+bool IsFull(Queue *queue);
+/*
+- Returns true if the queue is full
+*/
+
+bool Enqueue(Queue *queue, double x);
+/*
+- Adds a new element with value x to the rear of the queue
+- Returns true if the operation is successful and false otherwise
+*/
+
+bool Dequeue(Queue *queue, double *x);
+/*
+- Removes an element from the front of the queue
+- Returns true if the operation is successful and false otherwise
+- Passes the value of the front element to x
+*/
+
+void DisplayQueue(Queue *queue);
+
+void DestroyQueue(Queue *queue);
+/*
+- Free the memory occupied by the queue
+*/
