97ecae48dda9b89158ab7b520b9b9b2a3e64d64e
dengrenjie <dengrenjie31@gmail.com>
2018-04-22 12:03:52 +0800

    merge

---
+++ Lab9/BST.cpp
@@ -0,0 +1,19 @@
+#include "BST.h"
+#include "stdlib.h"
+
+Node* insertNode(Node **proot, int x){
+	// insert your code here and remove the "return 0" statement
+	return 0;
+}
+Node* deleteNode(Node **proot, int x){
+	// insert your code here and remove the "return 0" statement
+	return 0;
+}
+Node* findNode(Node *root, int x){
+	// insert your code here and remove the "return 0" statement
+	return 0;
+}
+
+void destroyTree(Node *root){
+	// insert your code here
+}
