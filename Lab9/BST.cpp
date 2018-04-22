253e6c4ca59a141654a78a3d75bbbfa779a6648c
Renjie Deng <dengrenjie31@gmail.com>
2018-04-22 12:37:07 +0800

    Lab9: Done

--- Lab9/BST.cpp
+++ Lab9/BST.cpp
@@ -1,9 +1,15 @@
+//
+// Binary Search Tree
+// Author: Renjie Deng
+// Created on: April 22nd
+//
+
 #include "BST.h"
 #include <stdlib.h>
 
 Node* insertNode(Node **proot, int x){
     Node *current = *proot;//Record the current position of pointer
-    if (current == NULL) {
+    if (current == NULL) {//Check if the first node is null
         *proot = (Node *)malloc(sizeof(Node));
         current = *proot;
         current->key = x;
@@ -12,8 +18,9 @@ Node* insertNode(Node **proot, int x){
         return current;
     }
     while (current != NULL) {
-        if (x == current->key)
+        if (x == current->key)//Stop insert if same element exists
             break;
+        //Find the position to insert
         if (x < current->key) {
             if (current->left != NULL) {
                 current = current->left;
@@ -41,10 +48,10 @@ Node* insertNode(Node **proot, int x){
 }
 
 Node* deleteNode(Node **proot, int x){
-    Node *deleteNode = findNode(*proot, x);
-    if (deleteNode != NULL) {
+    Node *deletedNode = findNode(*proot, x);//Confirm the existance of node to delete
+    if (deletedNode != NULL) {//Check if node to delete exists
         Node *present = *proot;//Find the previous node of the deleted node
-        if (x == present->key) {//When the root is deleteNode
+        if (x == present->key) {//When the root is the deleteNode
             if (present->right != NULL) {
                 present = present->right;
                 if (present->left != NULL) {
@@ -53,23 +60,42 @@ Node* deleteNode(Node **proot, int x){
                     *proot = present->left;
                     present->left = NULL;
                     present = *proot;
-                    present->left = deleteNode->left;
-                    present->right = deleteNode->right;
-                    return deleteNode;
+                    present->left = deletedNode->left;
+                    present->right = deletedNode->right;
+                    return deletedNode;
                 }
                 *proot = present;
-                present->left = deleteNode->left;
-                return deleteNode;
+                present->left = deletedNode->left;
+                return deletedNode;
             }
             *proot = present->left;
-            return deleteNode;
+            return deletedNode;
         }
-        while (present->left->key != x && present->right->key != x) {
-            present = x < present->key ? present->left : present->right;
+        while (true) {//Find the previous node of the deleteNode
+            if (present->left != NULL && present->right != NULL) {
+                if (present->left->key != x && present->right->key != x) {
+                    present = x < present->key ? present->left : present->right;
+                    continue;
+                }
+            }
+            else if (present->left != NULL && present->right == NULL) {
+                if (present->left->key != x) {
+                    present = present->left;
+                    continue;
+                }
+            }
+            else if (present->left == NULL && present->right != NULL) {
+                if (present->right->key != x) {
+                    present = present->right;
+                    continue;
+                }
+            }
+            break;
         }
         Node *nextNode;//Record the next node which replace the position of deleted node
-        if (deleteNode->right != NULL) {
-            nextNode = deleteNode->right;
+        //Replace the deletedNode with another node
+        if (deletedNode->right != NULL) {
+            nextNode = deletedNode->right;
             if (nextNode->left != NULL) {
                 while (nextNode->left->left != NULL)
                     nextNode = nextNode->left;
@@ -77,32 +103,32 @@ Node* deleteNode(Node **proot, int x){
                     present->left = nextNode->left;
                     nextNode->left = NULL;
                     nextNode = present->left;
-                    nextNode->left = deleteNode->left;
-                    nextNode->right = deleteNode->right;
-                    return deleteNode;
+                    nextNode->left = deletedNode->left;
+                    nextNode->right = deletedNode->right;
+                    return deletedNode;
                 }
                 else {
                     present->right = nextNode->left;
                     nextNode->left = NULL;
                     nextNode = present->right;
-                    nextNode->left = deleteNode->left;
-                    nextNode->right = deleteNode->right;
-                    return deleteNode;
+                    nextNode->left = deletedNode->left;
+                    nextNode->right = deletedNode->right;
+                    return deletedNode;
                 }
             }
             if (x < present->key)
                 present->left = nextNode;
             else
                 present->right = nextNode;
-            nextNode->left = deleteNode->left;
-            return deleteNode;
+            nextNode->left = deletedNode->left;
+            return deletedNode;
         }
         if (x < present->key)
             present->left = present->left->left;
         else
             present->right = present->right->left;
     }
-    return deleteNode;
+    return deletedNode;
 }
 
 Node* findNode(Node *root, int x){
